265d0bf3c504517e836c69baa6db8a6bbfe59d99
MaxSpecific <166578082+kevin2636@users.noreply.github.com>
2025-05-24 10:00:36 +0530

    Update Data.js

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -2,21 +2,21 @@ const data = {
     productData: [
         {
             id: 1,
-            img: "https://en.wikipedia.org/wiki/Pencil",
+            img: "https://imgs.search.brave.com/_bsAn7PUz3qntSROqV8TSIf4htA6jyz6T0-SUJDOF0o/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy8w/LzA4L1BlbmNpbHNf/aGIuanBn",
             title: "Pencil",
             desc: "highly useful for writing,drawing,etc.",
             price: "1",
         },
         {
             id: 2,
-            img: "https://en.wikipedia.org/wiki/Eraser",
+            img: "https://imgs.search.brave.com/YfJZSP8TDWeEDhdyOsOyq9L01NiCIe8ZyQdEFAbKP6A/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWdz/LnNlYXJjaC5icmF2/ZS5jb20vNHJrREps/TDBOd1dzbmhfbWwx/eUZBaTUtWUZBUjI1/XzJJalJqN0xTdTlO/Zy9yczpmaXQ6NTAw/OjA6MDowL2c6Y2Uv/YUhSMGNITTZMeTkx/Y0d4di9ZV1F1ZDJs/cmFXMWxaR2xoL0xt/OXlaeTkzYVd0cGNH/VmsvYVdFdlkyOXRi/Vzl1Y3k4dy9MekJt/TDBaaFltVnlYME5o/L2MzUmxiR3hmUlhK/aGMyVnkvY3k1cWNH/Yw",
             title: "Eraser",
             desc: "Highly efficient in erasing pencil",
             price: "2",
         },
         {
             id: 3,
-            img: "https://www.pentel.com/collections/pens",
+            img: "https://imgs.search.brave.com/d0tSiFXLMktBkiavF-Pgw-hGf3Gg_oCygEVBf-oM8v0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWdz/LnNlYXJjaC5icmF2/ZS5jb20vNUlKa2Jv/LXhtcmp4anVkV01f/c00xSHd3bjJTYURq/UzRvQVVtRWdZZTZE/VS9yczpmaXQ6NTAw/OjA6MDowL2c6Y2Uv/YUhSMGNITTZMeTkz/ZDNjdS9jR1Z1YUdW/aGRtVnVMbU52L0xu/VnJMMjFsWkdsaEwy/TmgvZEdGc2IyY3Zj/SEp2WkhWai9kQzlq/TDNJdlkzSnZjM010/L1kyeHBZMnN0YldG/MGRHVXQvWW14aFky/c3RZbUZzYkhCdi9h/VzUwTFhCbGJpNXFj/R2NfL2NYVmhiR2ww/ZVQwNE1DWmkvWnkx/amIyeHZjajB5TlRV/cy9NalUxTERJMU5T/Wm1hWFE5L1ltOTFi/bVJ6Sm1obGFXZG8v/ZEQwME1EUW1kMmxr/ZEdnOS9Nekl3Sm1O/aGJuWmhjejB6L01q/QTZOREEw",
             title: "Pen",
             desc: "Simply better than a pencil,undefeated by eraser",
             price: "3",
@@ -26,3 +26,4 @@ const data = {
 }
 
 export default data;
+
