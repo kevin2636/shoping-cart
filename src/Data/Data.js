1a7c75afaaa6df4a3a9fbea4499e37028f539bde
MaxSpecific <166578082+kevin2636@users.noreply.github.com>
2025-05-24 09:53:10 +0530

    Update Data.js

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -2,24 +2,24 @@ const data = {
     productData: [
         {
             id: 1,
-            img: "https://i0.wp.com/parakkatjewels.com/wp-content/uploads/2022/06/PEBA012-190-SITE-1.jpg?fit=996%2C996&ssl=1",
-            title: "ENAMEL BANGLE PEBA012",
-            desc: "Model: PEBA012 Brand, Manufacture, Warranty by Parakkat Size Adjustable Product...",
-            price: "2299",
+            img: "https://en.wikipedia.org/wiki/Pencil",
+            title: "Pencil",
+            desc: "highly useful for writing,drawing,etc.",
+            price: "1",
         },
         {
             id: 2,
-            img: "https://i0.wp.com/parakkatjewels.com/wp-content/uploads/2022/02/15A.webp?fit=2500%2C2500&ssl=1",
-            title: "BRACELET BANGLE",
-            desc: "Model: PEBA012 Brand, Manufacture, Warranty by Parakkat Size Adjustable Product ...",
-            price: "2420",
+            img: "https://en.wikipedia.org/wiki/Eraser",
+            title: "Eraser",
+            desc: "Highly efficient in erasing pencil",
+            price: "2",
         },
         {
             id: 3,
-            img: "https://i0.wp.com/parakkatjewels.com/wp-content/uploads/2022/06/PGBAH28-140-SITE.jpg?fit=1559%2C1559&ssl=1",
-            title: "Golden Bangle",
-            desc: "Model: PEBA012 Brand, Manufacture, Warranty by Parakkat Size Adjustable Product... ",
-            price: "1694",
+            img: "https://www.pentel.com/collections/pens",
+            title: "Pen",
+            desc: "Simply better than a pencil,undefeated by eraser",
+            price: "3",
         }
         
     ]
